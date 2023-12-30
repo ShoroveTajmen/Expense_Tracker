04e68cf1dc61007f7725589716cad6da98cbe14b
ShoroveTajmen <sorovitajmen@gmail.com>
2023-12-30 18:28:52 +0600

    basic set-up done

---
+++ expenseClient/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className='ml-[100px]'>expense-tracker</h1>
+    </>
+  )
+}
+
+export default App
---
+++ expenseClient/src/Pages/Home/SocialLogin.jsx
@@ -0,0 +1,10 @@
+
+const SocialLogin = () => {
+    return (
+        <div>
+            <h1>Social login page</h1>
+        </div>
+    );
+};
+
+export default SocialLogin;
