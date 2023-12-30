af8d965c53753797db034d21b235faaf4adb0f98
ShoroveTajmen <sorovitajmen@gmail.com>
2023-12-30 16:43:19 +0600

    basic set-up done

--- expense-client/src/Layout/MainLayout/MainLayout.jsx
+++ expense-client/src/Layout/MainLayout/MainLayout.jsx
@@ -1,11 +1,17 @@
-
+import { Outlet } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
+import Navbar from "../../Components/Navbar/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <h1>This is main layout</h1>
-        </div>
-    );
+  return (
+    <div className="max-w-[1600px] mx-auto bg-[#FFF4F4]">
+      <Navbar></Navbar>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
--- expense-client/src/Route/Route.jsx
+++ expense-client/src/Route/Route.jsx
@@ -1,11 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout/MainLayout";
+import Home from "../Pages/Home/Home";
 
 
-const Route = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const myCreateRoutes = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            }
+        ]
+    }
+])
 
-export default Route;
+export default myCreateRoutes;
