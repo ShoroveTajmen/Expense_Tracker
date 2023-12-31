d323989fa0ae6ac62c8150a28e6aec1035b3c019
ShoroveTajmen <sorovitajmen@gmail.com>
2023-12-31 19:57:37 +0600

    add expense form and functionality added

--- expenseClient/src/Pages/Dashboard/AddExpense.jsx
+++ expenseClient/src/Pages/Dashboard/AddExpense.jsx
@@ -1,10 +1,54 @@
+import moment from "moment";
+import { useState } from "react";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+
 const AddExpense = () => {
+  const axiosPublic = useAxiosPublic();
+  //for date picker
+  const [selectedDate, setSelectedDate] = useState();
+  const formattedDate = moment(selectedDate).format("DD/MM/YYYY");
+
+  const handleAddExpense = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const productTitle = form.productTitle.value;
+    const amount = form.amount.value;
+    const category = form.category.value;
+    const  notes = form.notes.value;
+
+    const newExpense = {
+      productTitle,
+      amount,
+      category,
+      formattedDate,
+      notes
+    };
+    console.log(newExpense);
+
+    axiosPublic.post("/expense", newExpense).then((res) => {
+      console.log(res.data);
+      if (res.data.insertedId) {
+        form.reset();
+        Swal.fire({
+          position: "middle",
+          icon: "success",
+          title: "New product added successfully!",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
   return (
     <div className="shadow-lg shadow-[#D0BFFF] p-24 lg:w-[1100px] mx-auto mt-12 mb-12 bg-white">
       <h2 className="text-4xl mb-8 lg:mb-0 font-bold uppercase text-[#756AB6]">
         Expense Record
       </h2>
-      <form>
+      <form onSubmit={handleAddExpense}>
         {/* product name and product img */}
         <div className="form-control mt-8">
           <input
@@ -24,11 +68,11 @@ const AddExpense = () => {
         </div>
         <div className="form-control mt-6">
           <select
-            name="countryCode"
+            name="category"
             className=" border-b-2 border-gray-500  w-full"
           >
             <option disabled selected>
-            Category
+              Category
             </option>
             <option>Eat Outs</option>
             <option>Gardening</option>
@@ -42,9 +86,18 @@ const AddExpense = () => {
           <input
             type="date"
             name="date"
+            selected={formattedDate}
+            onChange={(date) => setSelectedDate(date)}
             placeholder="Incurred on"
             className="border-b-2 border-gray-500  w-full placeholder-black"
           />
+          {/* <DatePicker
+            className="border border-black"
+            selected={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+            // dateFormat="dd/MM/yyyy"
+            minDate={new Date()}
+          ></DatePicker>{" "} */}
         </div>
         <div className="form-control mt-6 ">
           <input
@@ -59,8 +112,10 @@ const AddExpense = () => {
           value="SUBMIT"
           className=" btn  bg-[#1a3d74] font-bold text-white uppercase lg:w-[100px] mt-[50px]"
         />
-        <button className="btn uppercase bg-gray-300 font-bold ml-[700px]">Cancel</button>
       </form>
+      <button className="btn uppercase bg-gray-300 font-bold pl-[21px] pr-[21px] mt-[10px]">
+        Cancel
+      </button>
     </div>
   );
 };
